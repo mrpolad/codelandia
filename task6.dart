f240333e9db3e529a505202b1001b274c3228c53
Polad <78161316+mrpolad@users.noreply.github.com>
2023-05-13 19:16:52 +0400

    Update task6.dart

--- task6.dart
+++ task6.dart
@@ -1,8 +1,8 @@
 void main(List<String> args) {
-List list1=[46,24,20500];
-List list2=[120,3,53];
-List listTotal=list1+list2;
+List<int> list1=[46,24,20500];
+List<int> list2=[120,3,53];
+List<int> listTotal=list1+list2;
 print('Total list: ${listTotal}');
 listTotal.sort();
-print('Sorted list: ${listTotal }');
+print('Sorted list: ${listTotal}');
 }
