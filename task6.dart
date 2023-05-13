9c15c3d66dd7237f2ac8faeb727b81ccffc8c9d0
Polad <78161316+mrpolad@users.noreply.github.com>
2023-05-13 17:51:46 +0400

    Create task6.dart

---
+++ task6.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+List list1=[46,24,20500];
+List list2=[120,3,53];
+List listTotal=list1+list2;
+print('Total list: ${listTotal}');
+listTotal.sort();
+print('Sorted list: ${listTotal }');
+}
