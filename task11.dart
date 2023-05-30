93a1ada2a3c3e97eb249f90ff1bd104df44301c2
Polad <78161316+mrpolad@users.noreply.github.com>
2023-05-30 13:16:33 +0400

    Create task11.dart

---
+++ task11.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  List<String> list = [
+    'ASUS',
+    'Asus',
+    'asus',
+    'HP',
+    'hp',
+    'Hp',
+    'MSI',
+    'msi',
+    'Msi',
+    'Msi',
+    "test"
+  ];
+  products1(list);
+}
+
+products1(List list) {
+  Map<String, int> products = {};
+  for(String m in list) {
+    String optimallasdirilmisM = m.toUpperCase();
+    products[optimallasdirilmisM] = (products[optimallasdirilmisM] ?? 0) + 1;
+  }
+
+  print(products);
+}
